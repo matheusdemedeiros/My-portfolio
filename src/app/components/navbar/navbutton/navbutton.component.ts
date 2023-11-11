948471831d971cac790fb21e8b303490871deea4
Matheus <matheusdemedeiros200599@gmail.com>
2023-11-11 18:00:44 -0300

    feat: adiciona scroll-behavior

--- src/app/components/navbar/navbutton/navbutton.component.ts
+++ src/app/components/navbar/navbutton/navbutton.component.ts
@@ -1,12 +1,18 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbutton',
   templateUrl: './navbutton.component.html',
-  styleUrls: ['./navbutton.component.scss']
+  styleUrls: ['./navbutton.component.scss'],
 })
 export class NavbuttonComponent {
-
-  @Input() public text: string = "";
-
+  @Input() public text: string = '';
+  @Input() public scrollToElementId: string = '';
+  constructor(private viewportScroller: ViewportScroller) {}
+  public onClickScroll(): void {
+    if(this.scrollToElementId){
+      this.viewportScroller.scrollToAnchor(this.scrollToElementId);
+    }
+  }
 }
